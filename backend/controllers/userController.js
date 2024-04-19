cbf0e3825d096a5406228ea6bd5ca89153198119
Lukasz Lukawczyk Notebook Parabola Asus <lukasz.lukawczyk@gmail.com>
2024-04-19 19:26:54 +0200

    JWT Token, authMiddleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,6 +39,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -60,6 +61,7 @@ const loginUser = asyncHandler(async (req, res) => {
       _id: user.id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     });
   } else {
     res.status(400);
@@ -69,9 +71,20 @@ const loginUser = asyncHandler(async (req, res) => {
 
 // @desc    Get user data
 // @route   GET /api/users/me
-// @access  Public
+// @access  Privat
 const getMe = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "User Data" });
+  const { _id, name, email } = await User.findById(req.user.id);
+
+  res.status(200).json({
+    id: _id,
+    name,
+    email,
+  });
 });
+
+//Generate JWT
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "30d" });
+};
 
 module.exports = { registerUser, loginUser, getMe };
