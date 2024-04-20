f9c0d1a51eea70d6847c79a9c50e8037043f16f4
Lukasz Lukawczyk Notebook Parabola Asus <lukasz.lukawczyk@gmail.com>
2024-04-20 15:56:11 +0200

    Added: Logout

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
-import {FaSignInAlt, FaUser} from 'react-icons/fa';
+import { Link, Outlet, useNavigate } from 'react-router-dom';
+import {FaSignInAlt, FaUser, FaSignOutAlt} from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { logout, reset } from '../../features/auth/authSlice';
 
 
 function Header() {
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { user } = useSelector((state) => state.auth);
+
+    const onLogout = () => {
+        dispatch(logout());
+        dispatch(reset());
+        navigate('/');
+    }
+    //TODO:ButtonStyle
   return (
     <>
         <header className='flex justify-between p-4 items-center border-b-[1px] border-b-black'>
@@ -11,16 +23,25 @@ function Header() {
                 Logo
             </div>
             <ul className='flex gap-2 justify-center items-center'>
-                <li>
-                    <Link to={'/login'} className='flex gap-2 justify-center items-center'>
-                        <FaSignInAlt /> Login
-                    </Link>
-                </li>
-                <li>
-                    <Link to={'/register'} className='flex gap-2 justify-center items-center'>
-                        <FaUser /> Register
-                    </Link>
-                </li>
+                {user ? (<>
+                    <li>
+                        <button onClick={onLogout} className='flex gap-2 justify-center items-center'>
+                            <FaSignOutAlt /> Logout
+                        </button>
+                    </li>
+                </>) : (<>
+                    <li>
+                        <Link to={'/login'} className='flex gap-2 justify-center items-center'>
+                            <FaSignInAlt /> Login
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to={'/register'} className='flex gap-2 justify-center items-center'>
+                            <FaUser /> Register
+                        </Link>
+                    </li>
+                </>)}
+                
             </ul>
         </header>
         <Outlet />
